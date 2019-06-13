ab9791c9c538e3267571a3ad79e07112c077b715
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-06-13 08:04:01 +0200

    Add presentation

---
+++ presentation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace wg_net_redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine(File.ReadAllText("Intro.txt"));
+
+        }
+    }
+}
